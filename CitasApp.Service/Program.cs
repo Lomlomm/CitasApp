98202391a32324ea90c9f34f0efce25661b7853a
Lomlomm <vanessaserna60@gmail.com>
2023-11-23 00:13:55 -0600

    cambio ssl

--- CitasApp.Service/Program.cs
+++ CitasApp.Service/Program.cs
@@ -60,7 +60,7 @@ app.UseCors(builder => builder.AllowAnyHeader()
                                 .AllowAnyMethod()
                                 .WithOrigins("http://localhost:4200")); 
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
 app.UseAuthentication();
 
